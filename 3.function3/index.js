349c6772903d2e402d93c41b2b71ca2ff62aad31
mwleungac <57056434+mwleungac@users.noreply.github.com>
2019-11-09 15:09:59 +0800

    Create index.js

---
+++ 3.function3/index.js
@@ -0,0 +1,16 @@
+function alphabetSort(message){
+  var arr = message.split('');
+  var tmp;
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[i] > arr[j]){
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr.join('');
+}
+console.log(alphabetSort('hello')); // should return 'ehllo'
+
